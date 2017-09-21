a8c98ca21b9e63f73f481bf964102d53333613bc
Austin Tackaberry <austintackaberry@gmail.com>
2017-09-20 21:49:36 -0700

    fulfilled user stories

--- src/App.js
+++ src/App.js
@@ -45,8 +45,12 @@ class App extends Component {
       board: board,
       data: data,
       counterArr: [0],
-      dataset: [counter]
+      dataset: [counter],
+      stopStart: "Stop"
     };
+    this.handleStopStart = this.handleStopStart.bind(this);
+    this.handleClear = this.handleClear.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
@@ -154,120 +158,167 @@ class App extends Component {
   }
 
   componentDidUpdate() {
-    setTimeout(function () {
-      var counterElem = 0;
-      var board = this.state.board;
-      var dataset = this.state.dataset;
-      var data = this.state.data;
-      var counterArr = this.state.counterArr;
-      var numNeighbors;
-      for (let i = 0; i < 30; i++) {
-        for (let j = 0; j < 50; j++) {
-          if (i === 0) {
-            if (j === 0) {
-              numNeighbors =
-                board[i][j+1] +
-                board[i+1][j] +
-                board[i+1][j+1]
-              ;
+    if (this.state.stopStart === "Stop") {
+      setTimeout(function () {
+        var counterElem = 0;
+        var board = this.state.board;
+        var dataset = this.state.dataset;
+        var data = this.state.data;
+        var counterArr = this.state.counterArr;
+        var numNeighbors;
+        for (let i = 0; i < 30; i++) {
+          for (let j = 0; j < 50; j++) {
+            if (i === 0) {
+              if (j === 0) {
+                numNeighbors =
+                  board[i][j+1] +
+                  board[i+1][j] +
+                  board[i+1][j+1]
+                ;
+              }
+              else if (j === 49) {
+                numNeighbors =
+                  board[i][j-1] +
+                  board[i+1][j-1] +
+                  board[i+1][j]
+                ;
+              }
+              else {
+                numNeighbors =
+                  board[i][j-1] +
+                  board[i][j+1] +
+                  board[i+1][j-1] +
+                  board[i+1][j] +
+                  board[i+1][j+1]
+                ;
+              }
             }
-            else if (j === 49) {
-              numNeighbors =
-                board[i][j-1] +
-                board[i+1][j-1] +
-                board[i+1][j]
-              ;
-            }
-            else {
-              numNeighbors =
-                board[i][j-1] +
-                board[i][j+1] +
-                board[i+1][j-1] +
-                board[i+1][j] +
-                board[i+1][j+1]
-              ;
-            }
-          }
-          else if (i === 29) {
-            if (j === 0) {
-              numNeighbors =
-                board[i][j+1] +
-                board[i-1][j] +
-                board[i-1][j+1]
-              ;
-            }
-            else if (j === 49) {
-              numNeighbors =
-                board[i][j-1] +
-                board[i-1][j-1] +
-                board[i-1][j]
-              ;
-            }
-            else {
-              numNeighbors =
-                board[i][j-1] +
-                board[i][j+1] +
-                board[i-1][j-1] +
-                board[i-1][j] +
-                board[i-1][j+1]
-              ;
-            }
-          }
-          else {
-            if (j === 0) {
-              numNeighbors =
-                board[i-1][j] +
-                board[i-1][j+1] +
-                board[i][j+1] +
-                board[i+1][j] +
-                board[i+1][j+1]
-              ;
-            }
-            else if (j === 49) {
-              numNeighbors =
-                board[i-1][j-1] +
-                board[i-1][j] +
-                board[i][j-1] +
-                board[i+1][j-1] +
-                board[i+1][j]
-              ;
+            else if (i === 29) {
+              if (j === 0) {
+                numNeighbors =
+                  board[i][j+1] +
+                  board[i-1][j] +
+                  board[i-1][j+1]
+                ;
+              }
+              else if (j === 49) {
+                numNeighbors =
+                  board[i][j-1] +
+                  board[i-1][j-1] +
+                  board[i-1][j]
+                ;
+              }
+              else {
+                numNeighbors =
+                  board[i][j-1] +
+                  board[i][j+1] +
+                  board[i-1][j-1] +
+                  board[i-1][j] +
+                  board[i-1][j+1]
+                ;
+              }
             }
             else {
-              numNeighbors =
-                board[i-1][j-1] +
-                board[i-1][j] +
-                board[i-1][j+1] +
-                board[i][j-1] +
-                board[i][j+1] +
-                board[i+1][j-1] +
-                board[i+1][j] +
-                board[i+1][j+1]
-              ;
+              if (j === 0) {
+                numNeighbors =
+                  board[i-1][j] +
+                  board[i-1][j+1] +
+                  board[i][j+1] +
+                  board[i+1][j] +
+                  board[i+1][j+1]
+                ;
+              }
+              else if (j === 49) {
+                numNeighbors =
+                  board[i-1][j-1] +
+                  board[i-1][j] +
+                  board[i][j-1] +
+                  board[i+1][j-1] +
+                  board[i+1][j]
+                ;
+              }
+              else {
+                numNeighbors =
+                  board[i-1][j-1] +
+                  board[i-1][j] +
+                  board[i-1][j+1] +
+                  board[i][j-1] +
+                  board[i][j+1] +
+                  board[i+1][j-1] +
+                  board[i+1][j] +
+                  board[i+1][j+1]
+                ;
+              }
             }
-          }
-          if (board[i][j] === 0) {
-            if (numNeighbors === 3) {
-              board[i][j] = 1;
+            if (board[i][j] === 0) {
+              if (numNeighbors === 3) {
+                board[i][j] = 1;
+              }
             }
-          }
-          else {
-            counterElem++;
-            if (numNeighbors < 2 || numNeighbors > 3) {
-              board[i][j] = 0;
+            else {
+              counterElem++;
+              if (numNeighbors < 2 || numNeighbors > 3) {
+                board[i][j] = 0;
+              }
             }
           }
         }
+        dataset.push(counterElem);
+        counterArr.push(counterArr[counterArr.length-1]+1);
+        if (dataset.length > 50) {
+          dataset.shift();
+          counterArr.shift();
+        }
+        data["labels"] = counterArr;
+        data["datasets"][0]["data"] = dataset;
+        this.setState({counterArr:counterArr, dataset:dataset, board:board, data:data});
+      }.bind(this), 250);
+    }
+  }
+
+  handleStopStart() {
+    if (this.state.stopStart === "Stop") {
+      this.setState({stopStart: "Start"});
+    }
+    else {
+      this.setState({stopStart: "Stop"});
+    }
+  }
+
+  handleClear() {
+    var board = [];
+    for (let i = 0; i < 30; i++) {
+      board.push([]);
+      for (let j = 0; j < 50; j++) {
+        board[i].push(0);
       }
-      dataset.push(counterElem);
-      counterArr.push(counterArr[counterArr.length-1]+1);
-      if (dataset.length > 50) {
-        dataset.shift();
-        counterArr.shift();
-      }
-      data["labels"] = counterArr;
-      data["datasets"][0]["data"] = dataset;
-      this.setState({counterArr:counterArr, dataset:dataset, board:board, data:data});
-    }.bind(this), 250);
+    }
+    var dataset = [];
+    const data = {
+      labels: [0],
+      datasets: [
+        {
+          label: "Life Counter",
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          data: dataset,
+          maintainAspectRatio: false
+        }
+      ]
+    };
+    this.setState({board:board, dataset:dataset, data:data, counterArr:[0], stopStart:"Start"});
+  }
+
+  handleClick(i,j) {
+    var board = this.state.board;
+    board[i][j] = 1;
+    this.setState({board:board});
   }
 
   render() {
@@ -279,7 +330,7 @@ class App extends Component {
           row.push(<span className="red"></span>);
         }
         else {
-          row.push(<span className="black"></span>);
+          row.push(<span onClick={() => this.handleClick(i,j)} className="black"></span>);
         }
       }
       finalJSX.push(<div>{row}</div>);
@@ -289,6 +340,8 @@ class App extends Component {
         <div id="main">
           {finalJSX}
         </div>
+        <button onClick={this.handleStopStart}>{this.state.stopStart}</button>
+        <button onClick={this.handleClear}>Clear</button>
         <Line
         	data={this.state.data}
           width={60}
